e8087e8f9c06a39844bdbe2f44890425b3f93843
Yash <guptayash134@gmail.com>
2021-06-18 23:28:17 +0530

    sqrtx

---
+++ Binary_Search/69.Sqrt(x).cpp
@@ -0,0 +1,26 @@
+class Solution
+{
+public:
+    int mySqrt(int x)
+    {
+        int low = 1, high = x;
+        int res;
+        while (low <= high)
+        {
+            long mid = low + (high - low) / 2;
+            long k = mid * mid;
+            if (k == x)
+                return mid;
+            else if (k < x)
+            {
+                res = mid;
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+};
